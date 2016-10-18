e43cede61b344747f873ec8008b5849dcdb3d8af
Jeff Merrick <jeff@doejo.com>
2016-10-18 11:24:02 -0500

    Add homepage more style

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "recipes#index"
+  authenticated :user do
+    root to: "recipes#index", as: :authenticated_root
+  end
+
+
+  root "pages#home"
   resources :recipes, path: "/"
 end
