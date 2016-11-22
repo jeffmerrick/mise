3946e29b4859b590220fc541eb278af3d587b228
Jeff Merrick <jeff@doejo.com>
2016-11-22 16:11:34 -0600

    Change invite redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_filter :store_current_location, :unless => :devise_controller?
+
+  private
+
+  def store_current_location
+    if params[:redirect_to].present?
+      store_location_for(:user, params[:redirect_to])    
+    else
+      store_location_for(:user, request.url)
+    end    
+  end
 end
--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -1,4 +1,5 @@
 class InviteController < ApplicationController
+  skip_before_action :authenticate_user!
   before_action :find_user
 
   def show
